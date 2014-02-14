4eb4b3ab0c3ba09ddb186dea7b57c8bb8bbeb9dd
Alessandro Verlato <averlato@gmail.com>
2014-02-14 16:17:05 +0100

    Added with_flags scope

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -3,6 +3,7 @@ module MakeFlaggable
     belongs_to :flaggable, :polymorphic => true
     belongs_to :flagger, :polymorphic => true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
+    scope :with_flags, lambda { |flags| where(:flag => flags(&:to_s)) }
     scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
 
     # def flaggable
