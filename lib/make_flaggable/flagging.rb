2abd42c94cb2289d0cff5586594b472561105ea8
Alessandro Verlato <averlato@gmail.com>
2014-07-27 14:40:22 +0200

    Removed counter_cache

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -1,7 +1,7 @@
 module MakeFlaggable
   class Flagging < ActiveRecord::Base
-    belongs_to :flaggable, :polymorphic => true, counter_cache: true
-    belongs_to :flagger, :polymorphic => true, counter_cache: true
+    belongs_to :flaggable, :polymorphic => true#, counter_cache: true
+    belongs_to :flagger, :polymorphic => true#, counter_cache: true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
     scope :with_flags, lambda { |*flags| where(:flag => flags.flatten.map(&:to_s)) }
     scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
