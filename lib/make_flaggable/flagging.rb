1e32a5d9643502feea0185dcc11961e7932cf6b7
Alessandro Verlato <averlato@gmail.com>
2014-07-27 13:26:22 +0200

    Added missing row

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -4,5 +4,6 @@ module MakeFlaggable
     belongs_to :flagger, :polymorphic => true, counter_cache: true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
     scope :with_flags, lambda { |*flags| where(:flag => flags.flatten.map(&:to_s)) }
+    scope :with_flaggable, lambda { |flaggable| where(:flaggable_type => flaggable.class.name, :flaggable_id => flaggable.id) }
   end
 end
